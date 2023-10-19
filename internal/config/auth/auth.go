5c901d2df861ec74d15f701306f0f7e664b7b1fd
Micah Shackelford <micahdshackelford@gmail.com>
2023-10-18 20:09:30 -0500

    chore: gitkeep assets dirs

--- internal/config/auth/auth.go
+++ internal/config/auth/auth.go
@@ -1,20 +1,6 @@
 package auth
 
-import (
-	"crypto/ecdsa"
-	"crypto/x509"
-	"fmt"
-)
-
 type Auth struct {
 	AppKey  string `envconfig:"AUTH_APP_KEY"`
 	AdminID string `envconfig:"AUTH_ADMIN_ID"`
-}
-
-func (a *Auth) GetPrivateKey() *ecdsa.PrivateKey {
-	k, err := x509.ParseECPrivateKey([]byte(a.AppKey))
-
-	fmt.Println(err)
-
-	return k
 }
